6c46def9fc378a376c85df91079a1ef86105b2d6
Yuhyeon0516 <rladbgus0516@naver.com>
2024-03-15 20:54:05 +0900

    Created header.

---
+++ components/common/LogoHeader.tsx
@@ -0,0 +1,34 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+type LogoHeader = {
+  rightComponent: () => React.JSX.Element;
+};
+
+export default function LogoHeader({ rightComponent }: LogoHeader) {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("@/assets/images/logo.png")}
+        width={250}
+        height={50}
+        style={{
+          objectFit: "cover",
+          top: -5,
+        }}
+      />
+      {rightComponent()}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: 60,
+    paddingHorizontal: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+});
