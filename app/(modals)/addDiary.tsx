c99243519d90de33aa2357c1f059a15915695fc7
Yuhyeon0516 <rladbgus0516@naver.com>
2024-03-19 20:33:10 +0900

    bug fixed: keyboard avoiding view not working

--- app/(modals)/addDiary.tsx
+++ app/(modals)/addDiary.tsx
@@ -1,8 +1,9 @@
-import { Platform, ScrollView, StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, View } from "react-native";
 import React from "react";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import TitleCloseHeader from "@/components/common/TitleCloseHeader";
 import AddDiary from "@/components/addDiary/AddDiary";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 export default function AddDiaryPage() {
   const safeAreaInsets = useSafeAreaInsets();
@@ -20,13 +21,14 @@ export default function AddDiaryPage() {
         title="하루 추가"
         description="여러분의 하루를 사진과 함께 알려주세요!"
       />
-      <ScrollView
+      <KeyboardAwareScrollView
         showsVerticalScrollIndicator={false}
         showsHorizontalScrollIndicator={false}
         keyboardDismissMode="interactive"
+        keyboardShouldPersistTaps="handled"
       >
         <AddDiary />
-      </ScrollView>
+      </KeyboardAwareScrollView>
     </View>
   );
 }
