48aade2607766b87f2e62a5109022dab28a21204
Yuhyeon0516 <rladbgus0516@naver.com>
2024-03-20 18:17:56 +0900

    Removed share tab

--- utils/util.ts
+++ utils/util.ts
@@ -61,3 +61,78 @@ export function getCalendarColumnSize() {
 
   return (width - 40) / 7;
 }
+
+export function createUUID() {
+  const characterList = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+
+  let uuid = "";
+
+  for (let i = 0; i < 8; i += 1) {
+    uuid += characterList[Math.floor(Math.random() * characterList.length)];
+  }
+
+  return uuid;
+}
